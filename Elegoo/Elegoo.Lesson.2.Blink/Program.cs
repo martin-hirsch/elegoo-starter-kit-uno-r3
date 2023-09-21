7e3659439b48d560c6986c37b4c51e28b619a845
Martin Hirsch <mh@jamoin.de>
2023-09-21 16:24:14 +0200

    initial commit

---
+++ Elegoo/Elegoo.Lesson.2.Blink/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Device.Gpio;
+
+const int pin = 18;
+var controller = new GpioController();
+controller.OpenPin(pin, PinMode.Output);
+
+while (true)
+{
+    controller.Write(pin, PinValue.High);
+    Thread.Sleep(200);
+    controller.Write(pin, PinValue.Low);
+    Thread.Sleep(200);
+}
